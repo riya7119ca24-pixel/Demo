261ab8c5fef236214bad6daa0b214ede78ae8551
Riya <riya7119.ca24@chitkara.edu.in>
2025-12-01 10:29:53 +0530

    commit second

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helloo, IntelliJ! This is my first commit11.");
+        System.out.println("Helloo, IntelliJ! This is my second commit11.");
     }
 }
