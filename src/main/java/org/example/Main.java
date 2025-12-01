01d54fedacc7ea6a897c11d3d1f2ca4c04634c99
Riya <riya7119.ca24@chitkara.edu.in>
2025-12-01 10:19:32 +0530

    commit first

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
+package
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, IntelliJ! This is my first commit.");
+        System.out.println("Hello, IntelliJ! This is my first commit11.");
     }
 }
