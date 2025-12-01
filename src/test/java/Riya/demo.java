61bcac2e810c8188b174e073e053b33bb00957c2
Riya <riya7119.ca24@chitkara.edu.in>
2025-12-01 10:02:02 +0530

    commit first

---
+++ src/test/java/Riya/demo.java
@@ -0,0 +1,104 @@
+package Riya;
+//import java.util.ArrayList;
+//import java.util.LinkedList;
+//import java.util.Vector;
+//import java.util.HashSet;
+//import java.util.LinkedHashSet;
+//import java.util.TreeSet;
+//import java.util.PriorityQueue;
+//import java.util.ArrayDeque;
+//import java.util.LinkedList;
+//import java.util.HashMap;
+//import java.util.LinkedHashMap;
+//import java.util.TreeMap;
+//import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
+public class demo {
+    public static void main(String[] args) {
+//        ArrayList<String> list = new ArrayList<>();
+//        list.add("Apple");
+//        list.add("Banana");
+//        list.add("Mango");
+//        System.out.println("List elements: " + list);
+//        LinkedList<String> list = new LinkedList<>();
+//        list.add("Dog");
+//        list.add("Cat");
+//        list.add("Rabbit");
+//list.addFirst("Tiger");
+//        System.out.println("List elements: " + list);
+//        Vector<String> vector = new Vector<>();
+//        vector.add("Red");
+//        vector.add("Blue");
+//        vector.add("Green");
+//        System.out.println("Vector elements: " + vector);
+//        HashSet<String> set = new HashSet<>();
+//        set.add("Apple");
+//        set.add("Banana");
+//        set.add("Cherry");
+//        set.add("Apple");
+//        System.out.println("HashSet elements: " + set);
+//        LinkedHashSet<String> set = new LinkedHashSet<>();
+//        set.add("Red");
+//        set.add("Blue");
+//        set.add("Green");
+//        set.add("Red");
+//        System.out.println("LinkedHashSet elements: " + set);
+//        TreeSet<String> set = new TreeSet<>();
+//
+//        // Adding elements
+//        set.add("Banana");
+//        set.add("Apple");
+//        set.add("Cherry");
+//        set.add("Apple");
+//        System.out.println("TreeSet elements: " + set);
+//        PriorityQueue<Integer> pq = new PriorityQueue<>();
+//        pq.add(30);
+//        pq.add(10);
+//        pq.add(20);
+//        System.out.println("PriorityQueue: " + pq);
+//        ArrayDeque<String> deque = new ArrayDeque<>();
+//        deque.add("A");
+//        deque.add("B");
+//        deque.add("C");
+// System.out.println("Deque elements: " + deque);
+//        LinkedList<String> list = new LinkedList<>();
+//
+//        list.add("Apple");
+//        list.add("Banana");
+//        list.add("Cherry");
+//        System.out.println(list);
+//        HashMap<Integer, String> map = new HashMap<>();
+//        map.put(1, "Apple");
+//        map.put(2, "Banana");
+//        map.put(3, "Cherry");
+//        System.out.println(map);
+//        LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
+//        map.put(10, "Red");
+//        map.put(20, "Blue");
+//        map.put(30, "Green");
+//        System.out.println(map);
+//        TreeMap<Integer, String> map = new TreeMap<>();
+//        map.put(30, "Orange");
+//        map.put(10, "Apple");
+//        map.put(20, "Banana");
+//        System.out.println(map);
+//        Hashtable<Integer, String> table = new Hashtable<>();
+//        table.put(1, "Dog");
+//        table.put(2, "Cat");
+//        table.put(3, "Rabbit");
+//
+//        // Print the table
+//        System.out.println(table);
+
+        ConcurrentHashMap<Integer, String> map = new ConcurrentHashMap<>();
+        map.put(100, "Pen");
+        map.put(200, "Pencil");
+        map.put(300, "Eraser");
+        System.out.println(map);
+
+    }
+}
+
+
+
+
